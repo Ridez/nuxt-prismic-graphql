d008a0e3008614b6646a631e931c047d4922fc7a
Lukasz Lassota <lukaszlassota@Lukaszs-MacBook-Pro-2.local>
2019-10-31 01:39:18 +0100

    add posts page

--- pages/index.vue
+++ pages/index.vue
@@ -1,66 +1,75 @@
 <template>
   <section class="container">
-    <div>
-      <logo/>
-      <h1 class="title">
-        nuxt-prismic-graphql
-      </h1>
-      <h2 class="subtitle">
-        Nuxt Prismic examle website
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
-      </div>
-    </div>
+    <b-row class="posts-row">
+      <b-col class="post-col" sm="6" md="4" v-for="(post, index) in posts.data.allPosts.edges" :key="index">
+        <img class="post-img" :src="post.node.post_image.url">
+        <div class="wrap-cont">
+          <prismic-rich-text class="post-title" :field="post.node.title"/>
+        </div>
+      </b-col>
+    </b-row>
   </section>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+import { PrismicLink } from "apollo-link-prismic";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import ApolloClient from "apollo-client";
+import gql from "graphql-tag";
+
+const client = new ApolloClient({
+  link: PrismicLink({
+    uri: "https://your-repository-name.prismic.io/graphql",
+  }),
+  cache: new InMemoryCache()
+});
 
 export default {
-  components: {
-    Logo
+  async asyncData() {
+    try {
+      const posts = await client.query({
+        query: gql`
+        {
+          allPosts {
+            edges {
+              node {
+                title
+                desc
+                post_image
+              }
+            }
+          }
+        }
+      `
+      })
+
+      return {
+        posts
+      }
+    } catch(e) {
+      console.log(e)
+    }
   }
 }
 </script>
 
-<style>
-
-.container {
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
+<style lang="scss">
+  .posts-row {
+    margin-top: 100px;
+    .post-col {
+      margin-bottom: 30px;
+      .post-img {
+        width: 100%;
+      }
+      .wrap-cont {
+        background-color: #fafafa;
+        padding: 15px;
+        .post-title {
+          h1 {
+            font-size: 2rem;
+          }
+        }
+      }
+    }
+  }
 </style>
